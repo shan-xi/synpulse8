c9548e81f2ce72b057ac28061a620050c1b4a5df
spinliaomarais <spinliao@storemarais.com>
2023-11-29 21:39:23 +0800

    init springboot app

---
+++ src/test/java/com/synpulse8/ebanking/EBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synpulse8.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
